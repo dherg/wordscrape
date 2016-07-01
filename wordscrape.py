64b18087dce7399ba078ce3dc8b71e6b46511366
dherg <hergendavid@gmail.com>
2016-06-30 21:19:26 -0400

    cycling through keywords and target words, writing results to csv

--- wordscrape.py
+++ wordscrape.py
@@ -4,12 +4,15 @@
 #			(words we are looking for on the results page) (newline separated)
 
 # TODO:
+#		- cycle through useragents
+#		- time delay
 #		- better output format 
 #		- read from csv?
-#		- 
+#		- clean up code
 import requests
 from bs4 import BeautifulSoup
 import re
+import csv
 
 
 # from https://techblog.willshouse.com/2012/01/03/most-common-user-agents/
@@ -24,8 +27,8 @@ useragents = [
 	'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'
 	]
 
-def googlesearch(searchfor):                                                                                                                                                                                       
-    link = 'http://www.google.com/search?q={}'.format(searchfor)
+def googlesearch(query):                                                                                                                                                                                       
+    link = 'http://www.google.com/search?q={}'.format(query)
     # print(link)                                                                                                                            
     ua = {'User-Agent': useragents[0]}                                                                
     # payload = {'q': searchfor}                                                                                                                                                                                     
@@ -41,10 +44,9 @@ def getkeywords():
 		keywords.append(line[:-1]) # strips \n at end of term
 	return(keywords)
 
-def main():
-	keywords = getkeywords()
-	print('sending request for {}'.format(keywords[0]))
-	soup = BeautifulSoup(googlesearch(keywords[0]), 'html.parser')
+def getresults(query):
+	print('sending request for {}'.format(query))
+	soup = BeautifulSoup(googlesearch(query), 'html.parser')
 	print('parsing response')
 	response_st = soup.find_all('span', {'class' : 'st'})
 	response_r = soup.find_all('h3', {'class' : 'r'})
@@ -55,32 +57,47 @@ def main():
 	links = []
 	for link in response_r:
 		links.append(re.sub(regex, ' ', str(link)))
-
 	alltext = descriptions
 	alltext.extend(links)
-
 	alltext = ' '.join(alltext).lower()
-	print('searching for target words')
-	targets = [line.rstrip('\n') for line in open('targets.txt', 'r')]
+	return(alltext)
 
+def searchtargets(targets, alltext, resultsfilewriter):
+	print('searching for target words')
+	targetfound = []
 	for target in targets:
 		lowertarget = target.lower()
 		if lowertarget in alltext:
-			print('{} - {}'.format(target, True))
+			targetfound.append("True")
 		else:
-			print('{} - {}'.format(target, False))
-		
+			targetfound.append("False")
+	return(targetfound)
 
+def main():
+	keywords = getkeywords()
+	targets = [line.rstrip('\n') for line in open('targets.txt', 'r')]
 
+	# open csv file and print header line
+	resultsfile = open('ScrapeResults.csv', 'w', encoding='utf8', newline='')
+	resultsfilewriter = csv.writer(resultsfile, quoting=csv.QUOTE_ALL)
+	header = ['Keywords']
+	for target in targets:
+		header.append(target)
+	# print(header)
+	resultsfilewriter.writerow(header)
 
-	with open('response.txt', 'r+') as responsefile:
-		# responsefile.write(soup.prettify())
-		pass
-
-
-
-
-
+	for keyword in keywords:
+		alltext = getresults(keyword)
+		searchresult = searchtargets(targets, alltext, resultsfile)
+		# print(searchresult)
+		# print(keyword)
+		row = [keyword]
+		# print(row)
+		row.extend(searchresult)
+		# print(row)
+		resultsfilewriter.writerow(row)
+	
+	
 
 
 
